a7ffc43d0b83e99b967dd676fd28c5080e912a4c
unknown <30213@yeyuguo.goldwind.org>
2015-11-15 15:20:53 +0800

    complete version

---
+++ stat_nc_h5.py
@@ -0,0 +1,25 @@
+# from stat_nc_hdf5 import stat
+from stat_nc_hdf5.stat import mean_PD_PM_PY as nc_h5
+def stat_mean(argList):
+    for year, domain, level in argList:
+        print year, domain, level
+        try:
+            mean_pt = nc_h5(year,domain,level)
+            # mean_pt.pdpmpy_mean()
+            # mean_pt.rose_dist_mean()
+            # mean_pt.wpdrose_mean()
+            mean_pt.hdf5()
+            print 'ok'
+        except Exception as e:
+            print e
+
+if __name__ =="__main__":
+    argList = []
+    for year in ['2013','2014']:
+        for domain in ['3','4','5','6','7']:
+            for level in ['10','70','80','100']:
+                argList.append((year, domain, level))
+    # for year in ['2013']:
+    #     for domain in ['3']:
+    #         for level in ['10', '70']:
+    stat_mean(argList)
